48070282d749a0bd54862799bc4b7719ba8bd1cc
Harilakshan <harirharilakshan@gmail.com>
2022-10-25 23:49:26 +0530

    courses

--- index.js
+++ index.js
@@ -1,28 +1,5 @@
-const express = require("express");
-const mongoose = require("mongoose");
-const app = express();
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
 
-const CourseModel = require("./models/courses");
-
-app.use(express.json());
-
-mongoose.connect(
-    "mongodb+srv://hari:%40Hari0712@crud.9w7qcbl.mongodb.net/courses?retryWrites=true&w=majority", {
-    useNewUrlParser: true,
-});
-
-app.get("/", async(req, res) =>{
-    const course = new CourseModel({courseId: "SCS 2208", courseName: "RAD", courseCredit: 3 });
-
-    try{
-        await course.save();
-        res.send("inserted data");
-    }catch(err){
-        console.log(err);
-    }
-    
-});
-
-app.listen(3001, () =>{
-    console.log("Server running on port 3001..");
-});
+ReactDOM.render(<App />, document.getElementById("root"));
